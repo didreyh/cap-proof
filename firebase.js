c0e80819cd017240c8533523dc23219d33a0a5f5
didreyh <xxcessyxx123@gmail.com>
2026-01-03 22:25:23 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,7 @@
-// firebase.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
+// firebase.js (Firebase v10 modular via CDN-friendly imports happen in the HTML files)
+// This file only exports your config so you can reuse it.
 
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: "AIzaSyCMuFIBYcEWJCknOsA-hRctc6I7v4Sr_gU",
   authDomain: "cap-proof.firebaseapp.com",
   projectId: "cap-proof",
@@ -12,8 +9,3 @@ const firebaseConfig = {
   messagingSenderId: "722260489486",
   appId: "1:722260489486:web:e4c8111afb86586f2fd35f"
 };
-
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
