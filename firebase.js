c3daca4c98ce5affd4704fbc685aee67f1e5eed1
didreyh <xxcessyxx123@gmail.com>
2026-01-03 17:30:30 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,17 @@
-// firebase.js (v8 compat, simplest for GitHub Pages)
+// firebase.js (ES Module)
+// ✅ Firebase config mo
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import {
+  getAuth,
+  setPersistence,
+  browserLocalPersistence,
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import {
+  getFirestore,
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+import {
+  getStorage,
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCMuFIBYcEWJCknOsA-hRctc6I7v4Sr_gU",
@@ -9,8 +22,11 @@ const firebaseConfig = {
   appId: "1:722260489486:web:e4c8111afb86586f2fd35f"
 };
 
-firebase.initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 
-const auth = firebase.auth();
-const db = firebase.firestore();
-const storage = firebase.storage();
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+
+// ✅ keep login kahit refresh
+await setPersistence(auth, browserLocalPersistence);
