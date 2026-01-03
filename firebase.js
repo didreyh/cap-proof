3fbd0481963020e8057d986953f5e9338b73fa1a
didreyh <xxcessyxx123@gmail.com>
2026-01-03 16:01:03 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,22 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+<script type="module">
+  // Firebase SDKs (browser CDN)
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+  import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+  import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+  import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCMuFIBYcEWJCknOsA-hRctc6I7v4Sr_gU",
-  authDomain: "cap-proof.firebaseapp.com",
-  projectId: "cap-proof",
-  storageBucket: "cap-proof.firebasestorage.app",
-  messagingSenderId: "722260489486",
-  appId: "1:722260489486:web:e4c8111afb86586f2fd35f"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyCMuFIBYcEWJCknOsA-hRctc6I7v4Sr_gU",
+    authDomain: "cap-proof.firebaseapp.com",
+    projectId: "cap-proof",
+    storageBucket: "cap-proof.firebasestorage.app",
+    messagingSenderId: "722260489486",
+    appId: "1:722260489486:web:e4c8111afb86586f2fd35f"
+  };
 
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
-export default app;
+  // Init
+  window.app = initializeApp(firebaseConfig);
+  window.auth = getAuth(app);
+  window.db = getFirestore(app);
+  window.storage = getStorage(app);
+</script>
