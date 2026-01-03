0e7b3f8993cf11eda42724f98ae993f7bc13cd2c
didreyh <xxcessyxx123@gmail.com>
2026-01-03 16:00:05 +0800

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,8 @@
-// firebase.js (ESM - works on GitHub Pages)
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-storage.js";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-// 🔥 Use your existing Firebase config (same as you posted)
 const firebaseConfig = {
   apiKey: "AIzaSyCMuFIBYcEWJCknOsA-hRctc6I7v4Sr_gU",
   authDomain: "cap-proof.firebaseapp.com",
@@ -14,7 +12,9 @@ const firebaseConfig = {
   appId: "1:722260489486:web:e4c8111afb86586f2fd35f"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+export default app;
